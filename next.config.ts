16722b4a29fb62fd6d164b3d171b5017fe268dc6
Shawna <shawna.yuan@ntdtv.com>
2025-07-21 13:52:26 -0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+    output: 'export', // 启用静态导出
+    basePath: '/learning-github-cicd', // 你的 GitHub 仓库名
+    assetPrefix: '/learning-github-cicd',
+}
 
 export default nextConfig;
