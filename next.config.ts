2f36e025916ead85d365c1580b1290a7c79ae475
wwiki <wwiki@users.noreply.github.com>
2025-07-21 18:57:07 +0000

    deploy: 86ef7e3bb8fcf2062df16d57327a3d3bc484ec83

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/learning-github-cicd',
-    assetPrefix: '/learning-github-cicd',
-};
-
-module.exports = nextConfig;
-
